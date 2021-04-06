9c8c3c411608e7b9de7cdc7292e4131f77a6bb91
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-05 23:42:04 -0300

    criando tabelas autores e posts do curso de banco de dados e mais comandos

---
+++ banco de dados/modulo_02/selecionando database e criando tabelas/criandoTabelas.sql
@@ -0,0 +1,22 @@
+/*
+use curso_banco_dados;
+CREATE  TABLE posts(
+ id int NOT  NULL  PRIMARY  key auto_increment,
+ titulo varchar(255) not null,
+ conteudo text(255) not NULL,
+ autor_id int not null 
+);
+show TABLES;
+*/
+use curso_banco_dados;
+/*
+CREATE  TABLE  autores(
+ id int not null PRIMARY  key auto_increment,
+ nome varchar(255) not NULL 
+
+);
+*/
+show tables;
+select * from autores;
+
+show columns from posts;
