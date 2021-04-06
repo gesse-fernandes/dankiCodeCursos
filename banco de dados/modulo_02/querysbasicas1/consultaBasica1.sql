9c9fa20a50615d22293246d9a43ea0bfed35fea9
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-06 08:12:02 -0300

    inserção de dados nas tabelas posts e autores e relacionamento entre id do autor para os posts

---
+++ banco de dados/modulo_02/querysbasicas1/consultaBasica1.sql
@@ -0,0 +1,15 @@
+use curso_banco_dados;
+
+
+
+/*CREATE TABLE posts(
+  id int NOT NULL PRIMARY key auto_increment,
+  titulo varchar(255) not null,
+  conteudo text(255) not NULL,
+  autor_id int ,
+  FOREIGN key(autor_id) REFERENCES autores(id)
+);*/
+/*INSERT  INTO autores values(null,"gesse");*/
+/*
+INSERT  into posts values(null,"meu primeiro post","meu conteudo do post",1);*/
+SELECT  * from autores;
