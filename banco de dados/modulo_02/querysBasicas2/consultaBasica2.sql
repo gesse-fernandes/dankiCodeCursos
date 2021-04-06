c20de30e375f8111ee24ffc5a35548a1ae12bff7
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-06 09:07:42 -0300

    add comando de update 'sendo -> update autores  set nome = 'alguma coisa where id = id do autor;''

---
+++ banco de dados/modulo_02/querysBasicas2/consultaBasica2.sql
@@ -0,0 +1,13 @@
+use curso_banco_dados;
+
+/*INSERT  into autores values(null,"dayane");*/
+/*
+SELECT  * from autores;
+*/
+/*
+insert into autores values(null,"teste");
+*/
+SELECT
+  *
+from
+  autores;
