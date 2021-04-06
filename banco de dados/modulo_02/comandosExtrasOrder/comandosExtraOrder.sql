72803ec6faff94e15caa46b4ac386780c57349ad
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-06 15:44:39 -0300

    comandos extras order by asc e desc

---
+++ banco de dados/modulo_02/comandosExtrasOrder/comandosExtraOrder.sql
@@ -0,0 +1,19 @@
+use curso_banco_dados;
+/*
+SELECT  conteudo from posts;
+*/
+/*
+INSERT  into posts values(null,"segundo posts","conteudo do 2 post",3);
+*/
+/*
+SELECT  * FROM posts;
+*/
+/*
+SELECT  * from posts ORDER BY id DESC;
+*/
+/*
+SELECT  * from posts ORDER BY  titulo desc;
+*/
+/*
+SELECT  * from posts ORDER BY titulo asc;
+*/
