aef2c803356cd84aebb0663ca48ae1decd439b25
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-06 15:30:51 -0300

    um pouco mais de comandos e comando do delete pelo nome e pelo id do autor

---
+++ banco de dados/modulo_02/querysBasicas3/consultaBasica3.sql
@@ -0,0 +1,12 @@
+use curso_banco_dados;
+
+/*
+INSERT  INTO  autores values(null,"zezinho");*/
+/*
+SELECT  * FROM  autores;
+*/
+/*
+DELETE  from autores WHERE  nome = 'zezinho' and id = 8;
+*/
+SELECT  nome FROM  autores;
+
