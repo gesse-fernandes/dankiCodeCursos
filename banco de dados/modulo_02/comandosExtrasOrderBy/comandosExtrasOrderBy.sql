e16266c668a7b540deb582626d54406408fc534c
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-06 16:13:03 -0300

    mais comandos group by e order by tanto desk quanto asc

---
+++ banco de dados/modulo_02/comandosExtrasOrderBy/comandosExtrasOrderBy.sql
@@ -0,0 +1,13 @@
+use curso_banco_dados;
+/*
+SELECT  * FROM  posts;
+*/
+/*
+INSERT INTO posts values(null,"ok1","ok2",4);
+*/
+/*
+SELECT  * FROM  posts GROUP BY autor_id;
+*/
+/*
+SELECT  * from posts GROUP BY  autor_id ORDER BY  id DESC;
+*/
