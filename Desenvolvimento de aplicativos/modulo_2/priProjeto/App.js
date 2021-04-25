c8fc24dc224f80950cb01a5b088ed0048acfd8fd
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-24 22:11:30 -0300

    um pouco mais sobre componentes View, Text e image inserir na url definindo largura  e altura

--- Desenvolvimento de aplicativos/modulo_2/priProjeto/App.js
+++ Desenvolvimento de aplicativos/modulo_2/priProjeto/App.js
@@ -1,27 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
-  
+
   render() {
-        return (
-    <View style={styles.container}>
-      <Text>Olá mundo</Text>
-      <StatusBar style="auto" />
-    </View>
+    return (
+         
+            <View >
+              <Text>Olá mundo</Text>
+        <Text>Olá mundo 2</Text>
+        <Text>Minha imagem:</Text>
+        <Image style= {{width:50, height:50}}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}/>
+            </View>
+                  
+            
+        
+        
   );
   }
 }
-
-
-  
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
