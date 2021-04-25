b71c6c1b2ed375d36777cbd4c3a40458d8c17c05
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-24 22:00:20 -0300

    crianndo uma classe app e chamando o metodo render pra renderisar nosso app

--- Desenvolvimento de aplicativos/modulo_2/priProjeto/App.js
+++ Desenvolvimento de aplicativos/modulo_2/priProjeto/App.js
@@ -2,15 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
+export default class App extends React.Component {
+  
+  render() {
+        return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Olá mundo</Text>
       <StatusBar style="auto" />
     </View>
   );
+  }
 }
 
+
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,3 +24,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
