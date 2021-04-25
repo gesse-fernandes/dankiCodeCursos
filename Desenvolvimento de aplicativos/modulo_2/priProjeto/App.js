dee3788a016821880bbdb08ac4cc4c75f93fd7b1
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-25 20:14:01 -0300

    um pouco mais sobre componentes mostrando o do tipo Header, Body e footer e criando um especifico do tipo botao e chamando na view

--- Desenvolvimento de aplicativos/modulo_2/priProjeto/App.js
+++ Desenvolvimento de aplicativos/modulo_2/priProjeto/App.js
@@ -1,26 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default class App extends React.Component {
-
+  Header = () => {
+    return (
+      <Text>Home| Sobre | Contato</Text>
+    );
+  }
+  Body = () => {
+    return (
+      <Text>Conteudo do meu app</Text>
+    );
+  }
+  Footer = () =>
+  {
+    return (
+      <Text>Aqui é meu rodape do meu aplicativo</Text>
+    );
+  }  
   render() {
     return (
-         
-            <View >
-              <Text>Olá mundo</Text>
-        <Text>Olá mundo 2</Text>
-        <Text>Minha imagem:</Text>
-        <Image style= {{width:50, height:50}}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}/>
-            </View>
-                  
-            
-        
-        
-  );
+
+      <View style={{ marginTop: 120 }}>
+        <this.Header></this.Header>
+        <this.Body></this.Body>
+        <this.Footer></this.Footer>
+        <Botao></Botao>
+      </View>
+
+
+
+
+    );
   }
 }
 
+const Botao = () => {
+  return (
+    <Button title= 'Clique aqui'></Button>
+   );
+ }
