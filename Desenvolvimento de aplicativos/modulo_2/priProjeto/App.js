4f007b54cfbca65c03d70a84cb092f496c4f1d5c
gesse-fernandes <gesse-fm@hotmail.com>
2021-04-27 17:14:05 -0300

    mais um pouco sobre estilos nos componentes no header e body e introduçãoao scroolview

--- Desenvolvimento de aplicativos/modulo_2/priProjeto/App.js
+++ Desenvolvimento de aplicativos/modulo_2/priProjeto/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button,ScrollView } from 'react-native';
 import Header from './Componentes/Header.js';
 import Body from './Componentes/Body.js';
 export default class App extends React.Component {
@@ -8,13 +8,10 @@ export default class App extends React.Component {
   render() {
     return (
 
-      <View style={{ marginTop: 120 }}>
+      <ScrollView style={{ marginTop: 20, flex:1 }}>
         <Header></Header>
         <Body></Body>
-      </View>
-
-
-
+      </ScrollView>
 
     );
   }
--- Desenvolvimento de aplicativos/modulo_2/priProjeto/Componentes/Body.js
+++ Desenvolvimento de aplicativos/modulo_2/priProjeto/Componentes/Body.js
@@ -1,11 +1,83 @@
 import React from 'react';
-import {Text,View} from 'react-native';
+import {Image, Text,View} from 'react-native';
 
 const Body = () => {
     return (
-        <View style= {{padding:30}}>
-        < Text > Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe. </Text>
+        <View>
+        <View style= {{flex:1,flexDirection:'row', flexWrap:'wrap',padding:30,justifyContent:'center'}}>
+        < Text  style={{width:'100%',paddingLeft:20,paddingRight:20,paddingTop:20}}> Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe. </Text>
+            < Image style = {
+                {
+                    width: 90,
+                    height: 90,
+                }
+            }
+            source = {
+                    {
+                uri: 'https://reactnative.dev/img/tiny_logo.png',
+                
+            }}/>
         </View>
+         <View style = {
+                 {
+                     flex: 1,
+                     flexDirection: 'row',
+                     flexWrap: 'wrap',
+                    padding: 30,
+                     justifyContent:'center'
+                 }
+             } >
+             <Text style = {
+                 {
+                     width: '100%',
+                     paddingLeft: 20,
+                        paddingRight: 20
+                     , paddingTop: 20
+                 }
+             }> Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe. </Text> <Image style = {
+                 {
+                     width: 90,
+                        height: 90,
+                     
+                 }
+             }
+         source = {
+             {
+                 uri: 'https://reactnative.dev/img/tiny_logo.png',
+
+             }
+         } /> </View>
+              <View style = {
+                      {
+                          flex: 1,
+                          flexDirection: 'row',
+                          flexWrap: 'wrap',
+                          padding: 30,
+                          justifyContent: 'center'
+                      }
+                  } >
+                  <Text style = {
+                      {
+                          width: '100%',
+                          paddingLeft: 20,
+                          paddingRight: 20,
+                          paddingTop: 20
+                      }
+                  } > Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit.Quisquam quae deleniti illum maxime, aspernatur distinctio!Ducimus debitis non, voluptas id porro enim, eius vel, totam necessitatibus optio eos autem saepe. </Text> <Image style = {
+                      {
+                          width: 90,
+                          height: 90,
+                      }
+                  }
+              source = {
+                  {
+                      uri: 'https://reactnative.dev/img/tiny_logo.png',
+
+                  }
+              } />
+            </View>
+            </View>
+     
     );
 }
 export default Body;
